b43ed0e9644997824c13366e14e23b162a61fe8a
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2023-12-29 11:54:47 +0530

    Update 2

--- src/Pages/Women.js
+++ src/Pages/Women.js
@@ -1,9 +1,70 @@
+import { Image, SimpleGrid, Tab, TabIndicator, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 import React from 'react'
 
 export default function Women() {
   return (
     <>
-        Hello from Women Section!
+        <Tabs isFitted variant='enclosed' p={3} colorScheme='teal'>
+          <TabList mb='1em'>
+            <Tab>Stitched</Tab>
+            <Tab>Unstitched</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+                    <SimpleGrid p={""} spacing={0} 
+                minChildWidth={{base:"150px",sm:"200px",md:"300px"}} 
+                mt={1} ml={"1%"} rowGap={5} columnGap={4}
+              >
+                
+                {/* Items */}
+                <Image 
+                  src='https://www.koskii.com/cdn/shop/files/koskii-wine-kashmirithreadwork-georgette-designer-salwar-suit-ssrm0030431_wine_3_1800x1800.jpg?v=1690976407'
+                  h={{base:"220px", sm: "340px",md:"420px",xl:"470px"}}
+                  borderRadius={20}
+                />
+                <Image 
+                  src='https://www.koskii.com/cdn/shop/files/koskii-wine-kashmirithreadwork-georgette-designer-salwar-suit-ssrm0030431_wine_3_1800x1800.jpg?v=1690976407'
+                  h={{base:"220px", sm: "340px",md:"420px",xl:"470px"}}
+                  borderRadius={20}
+                />
+                <Image 
+                  src='https://www.koskii.com/cdn/shop/files/koskii-wine-kashmirithreadwork-georgette-designer-salwar-suit-ssrm0030431_wine_3_1800x1800.jpg?v=1690976407'
+                  h={{base:"220px", sm: "340px",md:"420px",xl:"470px"}}
+                  borderRadius={20}
+                />
+                <Image 
+                  src='https://www.koskii.com/cdn/shop/files/koskii-wine-kashmirithreadwork-georgette-designer-salwar-suit-ssrm0030431_wine_3_1800x1800.jpg?v=1690976407'
+                  h={{base:"220px", sm: "340px",md:"420px",xl:"470px"}}
+                  borderRadius={20}
+                />
+                <Image 
+                  src='https://www.koskii.com/cdn/shop/files/koskii-wine-kashmirithreadwork-georgette-designer-salwar-suit-ssrm0030431_wine_3_1800x1800.jpg?v=1690976407'
+                  h={{base:"220px", sm: "340px",md:"420px",xl:"470px"}}
+                  borderRadius={20}
+                />
+                <Image 
+                  src='https://www.koskii.com/cdn/shop/files/koskii-wine-kashmirithreadwork-georgette-designer-salwar-suit-ssrm0030431_wine_3_1800x1800.jpg?v=1690976407'
+                  h={{base:"220px", sm: "340px",md:"420px",xl:"470px"}}
+                  borderRadius={20}
+                />
+                <Image 
+                  src='https://www.koskii.com/cdn/shop/files/koskii-wine-kashmirithreadwork-georgette-designer-salwar-suit-ssrm0030431_wine_3_1800x1800.jpg?v=1690976407'
+                  h={{base:"220px", sm: "340px",md:"420px",xl:"470px"}}
+                  borderRadius={20}
+                />
+                <Image 
+                  src='https://www.koskii.com/cdn/shop/files/koskii-wine-kashmirithreadwork-georgette-designer-salwar-suit-ssrm0030431_wine_3_1800x1800.jpg?v=1690976407'
+                  h={{base:"220px", sm: "340px",md:"420px",xl:"470px"}}
+                  borderRadius={20}
+                />
+                
+              </SimpleGrid> 
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
     </>
   )
 }
