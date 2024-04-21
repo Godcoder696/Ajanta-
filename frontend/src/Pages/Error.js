a69545531cd2e3567d1ec1adbc8451d7dc27ee2e
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-04-21 09:34:46 +0530

    Error Page Configured

--- frontend/src/Pages/Error.js
+++ frontend/src/Pages/Error.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Image, Text, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AjantaBlack from '../Assets/Logo/Ajanta_Sirsa-removebg.png';
 
 export default function Error() {
   return (
     <>
-        Error 
+      <VStack h={"100vh"} w="100vw" bgColor={"#e2e8f0"} justifyContent={"center"}>
+        <Link to={"/"} boxSize={{base:10,sm:11,md:12,lg:13}} >
+          <Image src={AjantaBlack} alt='Ajanta' boxSize={"80px"} />
+        </Link>
+        <Text fontSize={"30px"}>Page Not Found</Text>
+        <Link to={"/"}>
+          <Text cursor={"pointer"} fontWeight={"semibold"} fontSize={"17px"} className='err-redirection'>
+            Click Me!
+          </Text>
+        </Link>
+      </VStack>
     </>
   )
 }
