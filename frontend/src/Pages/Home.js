60cd65fe482b4a65fc8294a0d124f708049d83cc
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-05-03 10:08:16 +0530

    slider updated!

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -4,11 +4,12 @@ import Slider from './Components/Home/Slider';
 import Categories from './Components/Home/Categories';
 import BestSeller from './Components/Home/BestSeller';
 import Marque from './Components/Home/Marque';
+import Slider1 from '../Assets/Sliders/Slider1.webp';
+import Slider2 from '../Assets/Sliders/Slider2.webp';
 
 export default function Home() {
   const imgArr=[
-    {url:"https://i.pinimg.com/originals/d0/78/70/d078705c172a131d88c67bd19986172d.jpg"},
-    {url:"https://i.pinimg.com/originals/da/99/ce/da99ce38482fb78692583d255a1de64d.jpg"},
+    {url: Slider1},
     {url:"https://i.pinimg.com/originals/f3/66/6c/f3666c5ad466a64d0a64fa07debcdd3d.jpg"},
     {url:"https://i.pinimg.com/originals/d0/78/70/d078705c172a131d88c67bd19986172d.jpg"},
     {url:"https://i.pinimg.com/originals/da/99/ce/da99ce38482fb78692583d255a1de64d.jpg"},
