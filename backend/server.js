d91f1c71a85eb2f2f80cc75a3b04b4d5df5d228c
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-05-12 23:32:03 +0530

    refacter

--- backend/server.js
+++ backend/server.js
@@ -76,19 +76,19 @@ app.post('/all', async (req, res) => {
 
 // ---------------------------- DEPLOYMENT -------------------------------
 
-// const __dirname1 = path.resolve();
+const __dirname1 = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 
 // ---------------------------- DEPLOYMENT -------------------------------
